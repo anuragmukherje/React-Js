4c196e56955a8c1907c7bc2fca9f88a3d5911a58
Anurag Mukherjee <amdevremote@gmail.com>
2025-03-27 02:47:55 +0530

    Adding challenges codes

---
+++ challenges/challenge-1/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>
+            Welcome to my Website!!
+        </h1>
+        <nav>
+            <a href='/Home'>Home </a>
+            <br></br>
+            <a href='/About'>About </a>
+            <br></br>
+            <a href='/Contact'>Contact</a>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ challenges/challenge-1/src/components/MainContent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main>
+        <h2>
+            Main Content 
+        </h2>
+        <p>
+            By default, links will appear as follows in all browsers:
+            <ol>
+                <li>An unvisited link is underlined and blue</li>
+                <li>A visited link is underlined and purple</li>
+                <li>An active link is underlined and red</li>
+            </ol>
+        </p>
+    </main>
+  )
+}
+
+export default MainContent
