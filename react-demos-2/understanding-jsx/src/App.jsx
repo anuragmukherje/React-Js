2fb61857de23b52e9fb6efe0f2b8faa5c123f483
Anurag Mukherjee <amdevremote@gmail.com>
2025-03-27 03:14:15 +0530

    Pushing jsx-understanding module

---
+++ react-demos-2/understanding-jsx/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <section id='section'>
+      <h1>My Website</h1>
+      <article>
+        <h2>Welcome to React</h2>
+        <p className='text'>Paragraph Content</p>
+      </article>
+    </section>
+  )
+}
+
+export default App
